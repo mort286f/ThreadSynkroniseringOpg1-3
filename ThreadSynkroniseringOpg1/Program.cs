021ad3aa1cf72f4f43c2d58b1bc873343503f601
mortenvestergaard <57791299+mortenvestergaard@users.noreply.github.com>
2020-12-17 14:03:41 +0100

    Add files via upload

---
+++ ThreadSynkroniseringOpg1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSynkroniseringOpg1
+{
+    class Program
+    {
+        static int counter = 0;
+
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(CountUp);
+            thread1.Start();
+            Thread thread2 = new Thread(CountDown);
+            thread2.Start();
+            Console.Read();
+        }
+
+        static void CountUp()
+        {
+            while (true)
+            {
+                counter =+ counter + 2;
+                Console.WriteLine(counter);
+                Thread.Sleep(500);
+            }
+        }
+
+        static void CountDown()
+        {
+            while (true)
+            {
+                counter =+ counter - 1;
+                Console.WriteLine(counter);
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ ThreadSynkroniseringOpg2-3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSynkroniseringOpg2_3
+{
+    class Program
+    {
+        static int signCount = 0;
+        static object lockObj = new object();
+
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(WriteStars);
+            thread1.Start();
+            Thread thread2 = new Thread(WriteHashSigns);
+            thread2.Start();
+            Console.Read();
+        }
+
+        static void WriteStars()
+        {
+            while (true)
+            {
+                lock (lockObj)
+                {
+                    signCount = +signCount + 60;
+                    Console.WriteLine("************************************************************ " + signCount);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        static void WriteHashSigns()
+        {
+            while (true)
+            {
+                lock (lockObj)
+                {
+                    signCount = +signCount + 60;
+                    Console.WriteLine("############################################################ " + signCount);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+    }
+}
